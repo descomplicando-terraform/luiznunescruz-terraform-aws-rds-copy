81e79b4104bebead11d59079648dee15c6bd8149
Luiz Antonio Nunes da Cruz <lanunesdacruz@gmail.com>
2024-10-21 15:43:15 -0300

    update readme e variables

--- variables.tf
+++ variables.tf
@@ -1,27 +1,35 @@
-################################################################################
-#############             SOURCE VARIABLES                    ##################
-################################################################################
-variable "region_source" {}
+variable "region_source" {
+  description = "Região do RDS que será copiado"
+}
 
-variable "aws_profile_source" {}
+variable "aws_profile_source" {
+  default     = "default"
+  description = "Profile da AWS que será utilizado."
+}
 
-variable "db_instance_identifier_source" {}
+variable "db_instance_identifier_source" {
+  description = "Nome da instância RDS que será copiada."
+}
 
-################################################################################
-#############             DESTINATION VARIABLES               ##################
-################################################################################
+variable "region_destination" {
+  description = "Região para onde o RDS será copiado"
+}
 
-variable "region_destination" {}
-
-variable "db_instance_identifier_destination" {}
+variable "db_instance_identifier_destination" {
+  description = "Nome da instância para onde o RDS será copiado."
+}
 
 variable "instance_class_destination" {
   default     = ""
-  description = "Tipo de instância que será criada no destino. Ao deixar em branco usará o mesmo tipo de instância da fonte"
+  description = "Tipo de instância que será criada no destino. Ao deixar em branco usará o mesmo tipo de instância da fonte."
 }
 
 variable "db_subnet_group_name_destination" {
   default = ""
+  description = "Nome do Grupo de sub-redes para onde o RDS será copiado. Ao deixar em branco será criado no Grupo de sub-rede Padrão. Caso não exista será criado."
 }
 
-variable "subnet_ids_destination" {}
+variable "subnet_ids_destination" {
+  description = "Ids das VPC sub-redes para a criação de um Grupo de sub-redes para onde o RDS será copiado."
+
+}
